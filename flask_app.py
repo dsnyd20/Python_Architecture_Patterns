6c55504947fdb64f0732d4702062a170ea021dab
Daniel Snyder <snyder.danielj@gmail.com>
2023-01-20 12:12:23 -0600

    Updated flask app with services

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import services
 
 import config
 import model
@@ -14,11 +15,14 @@ app = Flask(__name__)
 @app.route("/allocate", methods=["POST"])
 def allocate_endpoint():
     session = get_session()
-    batches = repository.SqlAlchemyRepository(session).list()
+    repo = repository.SqlAlchemyRepository(session)
     line = model.OrderLine(
         request.json["orderid"], request.json["sku"], request.json["qty"],
     )
 
-    batchref = model.allocate(line, batches)
+    try:
+        batchref = services.allocate(line, repo, session)
+    except (model.OutOfStock, services.InvalidSku) as e:
+        return {"message": str(e)}, 400
 
     return {"batchref": batchref}, 201
