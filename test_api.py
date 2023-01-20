c512863f39a804a30f839f568a38922b7d6fba0e
Daniel Snyder <snyder.danielj@gmail.com>
2023-01-20 12:23:58 -0600

    Refactored api tests

--- test_api.py
+++ test_api.py
@@ -4,7 +4,7 @@ import config
 import uuid
 
 @pytest.mark.usefixtures("restart_api")
-def test_api_returns_allocation(add_stock):
+def test_happy_path_return_201_and_allocated_batch(add_stock):
     sku, othersku = random_sku(), random_sku("other")
     earlybatch = random_batchref(1)
     laterbatch = random_batchref(2)
@@ -25,26 +25,13 @@ def test_api_returns_allocation(add_stock):
     assert r.json()["batchref"] == earlybatch
 
 @pytest.mark.usefixtures("restart_api")
-def test_allocations_are_persisted(add_stock):
-    sku = random_sku()
-    batch1, batch2 = random_batchref(1), random_batchref(2)
-    order1, order2 = random_orderid(1), random_orderid(2)
-    add_stock(
-        [(batch1, sku, 10, "2011-01-01"), (batch2, sku, 10, "2011-01-02"),]
-    )
-    line1 = {"orderid": order1, "sku": sku, "qty": 10}
-    line2 = {"orderid": order2, "sku": sku, "qty": 10}
+def test_unhappy_path_returns_400_and_error_message():
+    unknown_sku, orderid = random_sku(), random_orderid()
+    data = {"orderid": orderid, "sku": unknown_sku, "qty": 20}
     url = config.get_api_url()
-
-    # first order uses up all stock in batch 1
-    r = requests.post(f"{url}/allocate", json=line1)
-    assert r.status_code == 201
-    assert r.json()["batchref"] == batch1
-
-    # second order should go to batch 2
-    r = requests.post(f"{url}/allocate", json=line2)
-    assert r.status_code == 201
-    assert r.json()["batchref"] == batch2
+    r = requests.post(f"{url}/allocate", json=data)
+    assert r.status_code == 400
+    assert r.json()["message"] == f"Invalid sku {unknown_sku}"
 
 def random_suffix():
     return uuid.uuid4().hex[:6]
